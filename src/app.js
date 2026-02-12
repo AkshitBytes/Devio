fd979307334a9dc255e356753aaf6b70fc47173d
AkshitBytes <akshitKumar129@gmail.com>
2026-02-12 20:40:34 +0530

    call the routes in app

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import express from "express"
+import authroutes from "./routes/auth.routes.js"
+
+const app = express()
+app.use(express.json())
+app.use("/api/auth",authroutes)
+export default app
