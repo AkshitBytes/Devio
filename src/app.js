55311e9a32381c9a71af57292982f71386c90459
AkshitBytes <akshitKumar129@gmail.com>
2026-02-16 16:54:07 +0530

    use cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express"
 import authroutes from "./routes/auth.routes.js"
+import cors from "cors";
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use("/api/auth",authroutes)
 export default app
