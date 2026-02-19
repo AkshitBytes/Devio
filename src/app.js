35f890bbbeaccf876590068f8130a51d82af9f76
AkshitBytes <akshitKumar129@gmail.com>
2026-02-19 17:54:11 +0530

    stiched everything

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express"
 import authroutes from "./routes/auth.routes.js"
+import dashboardroutes from "./routes/dashboard.routes.js"
+import codeRoutes from "./routes/code.routes.js"
 import cors from "cors";
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use("/auth",authroutes)
+app.use("/dashboard",dashboardroutes)
+app.use("/code",codeRoutes)
 export default app
